9ccba72763023b4dac22938181056e74365fbb4b
Pascal Huerst <pascal.huerst@gmail.com>
2014-09-04 12:06:16 +0200

    Added b.sh for dummys to build the modules out of three

---
+++ b.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ -z ${CROSS_COMPILE} ]]; then
+	echo You have to set \$CROSS_COMPILE in order to use this build script!
+	exit -1
+fi
+
+if [[ -z ${KSRC} ]]; then
+	echo You have to set \$KSRC to point to your kernel sourcetree, in order to usre this buildscript!
+	exit -1
+fi
+
+echo "Building..."
+echo "  \$KSRC=${KSRC}"
+echo "  \$CROSS_COMPILE=${CROSS_COMPILE}"
+
+make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} -C ${KSRC} M=$(pwd)
