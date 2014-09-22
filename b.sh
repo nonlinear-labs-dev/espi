18b6c153446899b2807a4a60fe9bbef7bb69864f
daniel <daniel.tzschentke@nonlinear-labs.de>
2014-09-22 17:55:14 +0200

    Enhanced b.sh

--- b.sh
+++ b.sh
@@ -10,8 +10,15 @@ if [[ -z ${KSRC} ]]; then
 	exit -1
 fi
 
-echo "Building..."
-echo "  \$KSRC=${KSRC}"
-echo "  \$CROSS_COMPILE=${CROSS_COMPILE}"
 
-make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} -C ${KSRC} M=$(pwd)
+if [[ $1 == "clean" ]]; then
+	echo "Cleaning..."
+	make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} -C ${KSRC} M=$(pwd) clean
+elif [[ $1 == "reload" ]]; then
+	scp espi_driver.ko root@192.168.7.2:/lib/modules/3.13.6/extra/espi_driver.ko
+	ssh -l root 192.168.7.2 "killall playground; rmmod espi_driver; modprobe espi_driver"
+else
+	echo "Building..."
+	make ARCH=arm CROSS_COMPILE=${CROSS_COMPILE} -C ${KSRC} M=$(pwd)
+fi
+
