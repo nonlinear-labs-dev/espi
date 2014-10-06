344f9bec6b23a91a433e87b5d2059c7d27b04605
nemanja <nemanja@nonlinearlabs.de>
2014-10-06 16:33:56 +0200

    ..

--- espi_driver.c
+++ espi_driver.c
@@ -1616,7 +1616,7 @@ static s32 espi_driver_probe(struct spi_device *dev)
 	espi_driver_buttons_setup(sb);
 	espi_driver_leds_setup(sb);
 	espi_driver_rb_leds_setup(sb);
-	espi_driver_ssd1305_setup(sb);
+	//espi_driver_ssd1305_setup(sb);
 	espi_driver_ssd1322_setup(sb);
 	espi_driver_encoder_setup(sb);
 
