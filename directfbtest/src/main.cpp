a8e3716d0a08f72a72b272bd7ea1a60e4e73801b
nemanja <nemanja@nonlinearlabs.de>
2015-03-23 16:27:03 +0100

    edited example

--- directfbtest/src/main.cpp
+++ directfbtest/src/main.cpp
@@ -20,6 +20,7 @@ static int screen_height = 0;
 
 int main (int argc, char **argv)
 {
+	unsigned char yy;
  
   DFBSurfaceDescription dsc;
   DFBCHECK (DirectFBInit (&argc, &argv));
@@ -31,16 +32,20 @@ int main (int argc, char **argv)
   
   DFBCHECK (dfb->CreateSurface( dfb, &dsc, &primary ));
   DFBCHECK (primary->GetSize (primary, &screen_width, &screen_height));
+
+  while(1) {
+	  printf("\nY:");
+	  scanf("%d",&yy);
+	  if(yy >= 64)
+		break;
+	  DFBCHECK (primary->SetColor (primary, 0x00, 0x00, 0x00, 0xff));
+	  DFBCHECK (primary->FillRectangle (primary, 0, 0, screen_width, screen_height));
+	  DFBCHECK (primary->SetColor (primary, 0x80, 0x80, 0xff, 0xff));
+	  DFBCHECK (primary->DrawLine (primary, 0, yy, screen_width - 1, yy));
+	  DFBCHECK (primary->Flip (primary, NULL, (DFBSurfaceFlipFlags)0));
+	  sleep (5);
+  }
   
-  printf("width: %d, height: %d\n", screen_width, screen_height);
-  
-  DFBCHECK (primary->FillRectangle (primary, 0, 0, screen_width, screen_height));
-  DFBCHECK (primary->Flip (primary, NULL, (DFBSurfaceFlipFlags)0));
-  sleep (5);
-  DFBCHECK (primary->SetColor (primary, 0x80, 0x80, 0xff, 0xff));
-  DFBCHECK (primary->DrawLine (primary, 0, screen_height / 2, screen_width - 1, screen_height / 2));
-  DFBCHECK (primary->Flip (primary, NULL, (DFBSurfaceFlipFlags)0));
-  sleep (5);
   primary->Release( primary );
   dfb->Release( dfb );
   return 23;
